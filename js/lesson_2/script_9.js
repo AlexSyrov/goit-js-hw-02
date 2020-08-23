7354c1765d6ad4b277142d22ad98280cdfcf67a1
AlexSyrov <1854105@gmail.com>
2020-08-23 11:10:02 +0300

    Добил 9-ю задачу

--- js/lesson_2/script_9.js
+++ js/lesson_2/script_9.js
@@ -1,17 +1,28 @@
-function isLoginValid(login, min = 4, max = 16) {}
+const isLoginValid = function (login, min = 4, max = 16) {
+  return login.length < min || login.length > max ? false : true;
+};
 
-function isLoginUnique(allLogins, login) {
+const isLoginUnique = function (allLogins, login) {
   'use strict';
+  return allLogins.includes(login) ? false : true;
+};
 
-}
-
-function addLogin(allLogins, login) {
+const addLogin = function (allLogins, login) {
   'use strict';
   const SUCCESS = 'Логин успешно добавлен!';
   const REFUSAL = 'Такой логин уже используется!';
   const ERROR = 'Ошибка! Логин должен быть размером от 4 до 16 символов';
   let message;
-}
+  if (!isLoginValid(allLogins, login)) {
+    message = ERROR;
+  } else if (!isLoginUnique(allLogins, login)) {
+    message = REFUSAL;
+  } else {
+    allLogins.push(login);
+    message = SUCCESS;
+  }
+  return message;
+};
 
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 
